714327e6f8ec837e77334f2e5ca698ecd9606775
Jet Newbery <75400263+NewberyJ@users.noreply.github.com>
2021-09-27 05:40:47 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,280 @@
+
+
+function showRandomNumber(){
+    document.getElementById("views").innerHTML = rando(5, 1000000);
+}
+
+
+function showRandomNumber2(){
+    document.getElementById("views2").innerHTML = rando(5, 1000000);
+}
+
+
+function showRandomNumber3(){
+    document.getElementById("views3").innerHTML = rando(5, 1000000);
+}
+
+
+
+function showRandomNumber4(){
+    document.getElementById("views4").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber5(){
+    document.getElementById("views5").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber6(){
+    document.getElementById("views6").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber7(){
+    document.getElementById("views7").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber8(){
+    document.getElementById("views8").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber9(){
+    document.getElementById("views9").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber10(){
+    document.getElementById("views10").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber11(){
+    document.getElementById("views11").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber12(){
+    document.getElementById("views12").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber13(){
+    document.getElementById("views13").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber14(){
+    document.getElementById("views14").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber15(){
+    document.getElementById("views15").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber16(){
+    document.getElementById("views16").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber17(){
+    document.getElementById("views17").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber18(){
+    document.getElementById("views18").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber19(){
+    document.getElementById("views19").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber20(){
+    document.getElementById("views20").innerHTML = rando(5, 1000000);
+}
+
+function showRandomNumber21(){
+    document.getElementById("views21").innerHTML = rando(5, 1000000);
+}
+
+
+
+
+
+
+
+
+
+function showaltnumber(){
+    document.getElementById("time").innerHTML = rando(1, 7);
+}
+
+function showaltnumber2(){
+    document.getElementById("time2").innerHTML = rando(1, 7);
+}
+
+function showaltnumber3(){
+    document.getElementById("time3").innerHTML = rando(1, 7);
+}
+
+function showaltnumber4(){
+    document.getElementById("time4").innerHTML = rando(1, 7);
+}
+
+function showaltnumber5(){
+    document.getElementById("time5").innerHTML = rando(1, 7);
+}
+
+function showaltnumber6(){
+    document.getElementById("time6").innerHTML = rando(1, 7);
+}
+
+function showaltnumber7(){
+    document.getElementById("time7").innerHTML = rando(1, 7);
+}
+
+function showaltnumber8(){
+    document.getElementById("time8").innerHTML = rando(1, 7);
+}
+
+function showaltnumber9(){
+    document.getElementById("time9").innerHTML = rando(1, 7);
+}
+
+function showaltnumber10(){
+    document.getElementById("time10").innerHTML = rando(1, 7);
+}
+
+function showaltnumber11(){
+    document.getElementById("time11").innerHTML = rando(1, 7);
+}
+
+function showaltnumber12(){
+    document.getElementById("time12").innerHTML = rando(1, 7);
+}
+
+function showaltnumber13(){
+    document.getElementById("time13").innerHTML = rando(1, 7);
+}
+
+function showaltnumber14(){
+    document.getElementById("time14").innerHTML = rando(1, 7);
+}
+
+function showaltnumber15(){
+    document.getElementById("time15").innerHTML = rando(1, 7);
+}
+
+function showaltnumber16(){
+    document.getElementById("time16").innerHTML = rando(1, 7);
+}
+
+function showaltnumber17(){
+    document.getElementById("time17").innerHTML = rando(1, 7);
+}
+
+function showaltnumber18(){
+    document.getElementById("time18").innerHTML = rando(1, 7);
+}
+
+function showaltnumber19(){
+    document.getElementById("time19").innerHTML = rando(1, 7);
+}
+
+function showaltnumber20(){
+    document.getElementById("time20").innerHTML = rando(1, 7);
+}
+
+function showaltnumber21(){
+    document.getElementById("time21").innerHTML = rando(1, 7);
+}
+
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
+addLoadEvent(showRandomNumber);
+
+addLoadEvent(showaltnumber);
+
+addLoadEvent(showRandomNumber2);
+
+addLoadEvent(showaltnumber2);
+
+addLoadEvent(showRandomNumber3);
+
+addLoadEvent(showaltnumber3);
+
+addLoadEvent(showRandomNumber4);
+
+addLoadEvent(showaltnumber4);
+
+addLoadEvent(showRandomNumber5);
+
+addLoadEvent(showaltnumber5);
+
+addLoadEvent(showRandomNumber6);
+
+addLoadEvent(showaltnumber6);
+
+addLoadEvent(showRandomNumber7);
+
+addLoadEvent(showaltnumber7);
+
+addLoadEvent(showRandomNumber8);
+
+addLoadEvent(showaltnumber8);
+
+addLoadEvent(showRandomNumber9);
+
+addLoadEvent(showaltnumber9);
+
+addLoadEvent(showRandomNumber10);
+
+addLoadEvent(showaltnumber10);
+
+addLoadEvent(showRandomNumber11);
+
+addLoadEvent(showaltnumber11);
+
+addLoadEvent(showRandomNumber12);
+
+addLoadEvent(showaltnumber12);
+
+addLoadEvent(showRandomNumber13);
+
+addLoadEvent(showaltnumber13);
+
+addLoadEvent(showRandomNumber14);
+
+addLoadEvent(showaltnumber14);
+
+addLoadEvent(showRandomNumber15);
+
+addLoadEvent(showaltnumber15);
+
+addLoadEvent(showRandomNumber16);
+
+addLoadEvent(showaltnumber16);
+
+addLoadEvent(showRandomNumber17);
+
+addLoadEvent(showaltnumber17);
+
+addLoadEvent(showRandomNumber18);
+
+addLoadEvent(showaltnumber18);
+
+addLoadEvent(showRandomNumber19);
+
+addLoadEvent(showaltnumber18);
+
+addLoadEvent(showRandomNumber20);
+
+addLoadEvent(showaltnumber20);
+
+addLoadEvent(showRandomNumber21);
+
+addLoadEvent(showaltnumber21);
